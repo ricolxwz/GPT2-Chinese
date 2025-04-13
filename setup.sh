f0f6c7b4e454fbd8fd861ede08c47592fb3e8377
wenzexu <ricol.xwz@outlook.com>
2025-04-13 08:47:22 +0800

    更新setup.sh以修正环境配置路径，改用conda管理Python环境和依赖

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 # 环境配置
-echo "export PATH="/usr/local/cuda/bin:\$PATH"" >> /home/wenzexu/.bashrc
-echo "export LD_LIBRARY_PATH="/usr/local/cuda/lib64:\$LD_LIBRARY_PATH"" >> /home/wenzexu/.bashrc
-echo "export GPT2_BASE_DIR=/home/wenzexu/GPT2-Chinese" >> /home/wenzexu/.bashrc
-echo "export CUDA_VISIBLE_DEVICES='0'" >> /home/wenzexu/.bashrc
-source /home/wenzexu/.bashrc
+echo "export PATH="/usr/local/cuda/bin:\$PATH"" >> /root/.bashrc
+echo "export LD_LIBRARY_PATH="/usr/local/cuda/lib64:\$LD_LIBRARY_PATH"" >> /root/.bashrc
+echo "export GPT2_BASE_DIR=/root/GPT2-Chinese" >> /root/.bashrc
+echo "export CUDA_VISIBLE_DEVICES='0'" >> /root/.bashrc
+source /root/.bashrc
 
 # 安装依赖
-pyenv install 3.12
-pyenv virtualenv 3.12 gpt2
-pyenv activate gpt2
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+conda create --name gpt2 python=3.12 -y
+conda activate gpt2
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1
 pip install -r requirements.txt
