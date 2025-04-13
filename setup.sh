acec3c360e335045b3745b26f31f346cf5f89952
wenzexu <ricol.xwz@outlook.com>
2025-04-13 08:52:39 +0800

    更新setup.sh，添加git代理配置以支持AutoDL环境

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ echo "export GPT2_BASE_DIR=/root/GPT2-Chinese" >> /root/.bashrc
 echo "export CUDA_VISIBLE_DEVICES='0'" >> /root/.bashrc
 source /root/.bashrc
 
+# 设置git代理(AutoDL)
+git config --global http.proxy http://172.26.1.26:12798
+git config --global https.proxy http://172.26.1.26:12798
+# git config --global --unset http.proxy
+# git config --global --unset https.proxy
+
 # 安装依赖
 conda create --name gpt2 python=3.12 -y
 conda activate gpt2
